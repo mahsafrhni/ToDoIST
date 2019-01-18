6339cec474030cef2a1a1c4023c64c5199057f0a
dena99 <38652032+dena99@users.noreply.github.com>
2019-01-18 21:50:24 +0330

    Create StartActivity.java

---
+++ app/src/main/java/com/example/todoist/StartActivity.java
@@ -0,0 +1,53 @@
+package com.example.dena.todolist;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button log_in, register;
+    TextView tx1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        log_in = findViewById(R.id.button);
+        register = findViewById(R.id.button2);
+        tx1 = findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(StartActivity.this, LoginActivity.class);
+                        new LoginActivity(i);
+                        finish();
+                    }
+                }, 0);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(StartActivity.this, RegisterActivity.class);
+                        new RegisterActivity(i);
+                        finish();
+                    }
+                }, 0);
+            }
+        });
+    }
+}
