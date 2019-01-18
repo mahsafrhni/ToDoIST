c54689b0daa45349a224251a6a1a546b018a2d2f
dena99 <38652032+dena99@users.noreply.github.com>
2019-01-18 21:51:32 +0330

    Update SplashActivity.java

--- app/src/main/java/com/example/todoist/SplashActivity.java
+++ app/src/main/java/com/example/todoist/SplashActivity.java
@@ -1,37 +1,35 @@
-package com.example.todoist;
+package com.example.dena.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private MediaPlayer splashSound;
+    TextView splash;
+    //private MediaPlayer splashSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        splash = findViewById(R.id.splash);
 
-        // splashSound = MediaPlayer.create(this, R.raw.sound);
-        // splashSound.start();
+       // splashSound = MediaPlayer.create(this, R.raw.sound);
+       // splashSound.start();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                Intent i = new Intent(SplashActivity.this, StartActivity.class);
                 startActivity(i);
                 finish();
             }
-
         },2000);
 
     }
