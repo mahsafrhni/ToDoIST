31652c4d03fd35737787afec0570ba9b957289fb
mahsafrhni <38652406+mahsafrhni@users.noreply.github.com>
2019-01-19 14:35:32 +0330

    Update StartActivity.java

--- app/src/main/java/com/example/todoist/StartActivity.java
+++ app/src/main/java/com/example/todoist/StartActivity.java
@@ -1,11 +1,10 @@
-package com.example.todoist;
-
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.todoist.R;
@@ -14,42 +13,30 @@ public class StartActivity extends AppCompatActivity {
 
     Button log_in, register;
     TextView tx1;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        log_in = findViewById(R.id.button);
-        register = findViewById(R.id.button2);
+        log_in = findViewById(R.id.login);
+        register = findViewById(R.id.register);
         tx1 = findViewById(R.id.textView3);
-        tx1.setVisibility(View.GONE);
+         tx1.setVisibility(View.GONE);
+         img = findViewById(R.id.imageView);
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
 
         log_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i = new Intent(StartActivity.this, LoginActivity.class);
-                        new LoginActivity();
-                        finish();
-                    }
-                }, 0);
-            }
-        });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i = new Intent(StartActivity.this, RegisterActivity.class);
-                        new RegisterActivity();
-                        finish();
-                    }
-                }, 0);
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
             }
         });
     }
 }
+
