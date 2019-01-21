086b30d5168c0d23c6582b562c64abe78f64e26f
mahsafrhni <38652406+mahsafrhni@users.noreply.github.com>
2019-01-21 17:06:04 +0330

    Update TasksActivity.java

--- app/src/main/java/com/example/todoist/TasksActivity.java
+++ app/src/main/java/com/example/todoist/TasksActivity.java
@@ -1,28 +1,40 @@
 package com.example.todoist;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 
 public class TasksActivity extends AppCompatActivity {
-    ListView tasks;
-    String[] myTasks;
+    //  ArrayList<Tasks> myTasks = new ArrayList<Tasks>();
+    // RestaurantAdapter adapter = null;
+    RecyclerView myRecycler;
     FloatingActionButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        tasks = findViewById(R.id.list_view);
-        myTasks = new String[]{"1", "2"};
-        add = findViewById(R.id.add);
-        ArrayAdapter<String> mAdapter =
-                new ArrayAdapter<String>(this, R.layout.list_item, R.id.list_view, myTasks);
+        myRecycler = findViewById(R.id.recycler_view);
+        // ListView tasks = (ListView) findViewById(R.id.list_view);
+        //  adapter = new RestaurantAdapter();
 
-        tasks.setAdapter(mAdapter);
+        //  myTasks.setAdapter(adapter);
+        add = findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TasksActivity.this, AddTask.class));
+            }
+        });
+
 
     }
 }
+
