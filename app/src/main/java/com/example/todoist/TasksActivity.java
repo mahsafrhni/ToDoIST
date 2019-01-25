5f51bbb322f63860ad16a3cf836cb8c70dc6c2f7
mahsafrhni <38652406+mahsafrhni@users.noreply.github.com>
2019-01-25 15:42:19 +0330

    Update TasksActivity.java

--- app/src/main/java/com/example/todoist/TasksActivity.java
+++ app/src/main/java/com/example/todoist/TasksActivity.java
@@ -1,40 +1,44 @@
 package com.example.todoist;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class TasksActivity extends AppCompatActivity {
-    //  ArrayList<Tasks> myTasks = new ArrayList<Tasks>();
-    // RestaurantAdapter adapter = null;
-    RecyclerView myRecycler;
+    ListView mList;
+    ArrayList<Tasks> Tasks;
     FloatingActionButton add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-        myRecycler = findViewById(R.id.recycler_view);
-        // ListView tasks = (ListView) findViewById(R.id.list_view);
-        //  adapter = new RestaurantAdapter();
-
-        //  myTasks.setAdapter(adapter);
+        setContentView(R.layout.activity_tasks);
+        mList = findViewById(R.id.list_view);
+        Tasks = new ArrayList<>();
+        TasksAdapter mAdapter = new TasksAdapter(this, Tasks);
+        //add kardan
+        mList.setAdapter(mAdapter);
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//kari k hengame click rooye radif ha anjam mishavad
+            }
+        });
         add = findViewById(R.id.add);
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(TasksActivity.this, AddTask.class));
+                Toast.makeText(TasksActivity.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+
             }
         });
-
-
     }
 }
 
