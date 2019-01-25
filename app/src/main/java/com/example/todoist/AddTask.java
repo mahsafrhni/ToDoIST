93a8fcb29559e34c7c717f8ee12bda1d003fffa4
mahsafrhni <38652406+mahsafrhni@users.noreply.github.com>
2019-01-25 15:41:01 +0330

    Update AddTask.java

--- app/src/main/java/com/example/todoist/AddTask.java
+++ app/src/main/java/com/example/todoist/AddTask.java
@@ -2,11 +2,12 @@ package com.example.todoist;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
@@ -17,31 +18,43 @@ import java.util.Locale;
 public class AddTask extends AppCompatActivity {
     public EditText title, date, time, priority, other;
     private final static int numberOfTasks = 1000;
-    ArrayList<String> tasks = (ArrayList<String>) getIntent().getSerializableExtra("tasks");
+    ArrayList<Tasks> tasks = new ArrayList<>();
     final Calendar myCalendar = Calendar.getInstance();
+    View view;
+    TextView textView;
+    //    int currentHour;
+//    int currentMinute;
+//    String amPm;
+//   currentHour = myCalendar.get(Calendar.HOUR_OF_DAY);
+//    currentMinute = myCalendar.get(Calendar.MINUTE);
+    TimePickerDialog timePickerDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        view = findViewById(R.id.view);
+        textView = findViewById(R.id.textView4);
         title = findViewById(R.id.title);
         date = findViewById(R.id.date);
         priority = findViewById(R.id.priority);
         other = findViewById(R.id.other);
         time = findViewById(R.id.time);
         String Title = title.getText().toString();
-        //String date = date.getText().toString();
+        String D = date.getText().toString();
         String Priority = priority.getText().toString();
         String Other = other.getText().toString();
-        Tasks task = new Tasks(Title, date, Priority, Other);
-        // tasks.add(task);
+        String Time = time.getText().toString();
+//        Tasks t = new Tasks(Title, D, Priority, Other);
+//        tasks.add(t);
         final DatePickerDialog.OnDateSetListener Date = new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
+            public void onDateSet(DatePicker view, int year, int month,
                                   int dayOfMonth) {
                 // TODO Auto-generated method stub
                 myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.MONTH, month);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 updateLabel();
             }
@@ -55,6 +68,7 @@ public class AddTask extends AppCompatActivity {
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
         time.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,4 +89,13 @@ public class AddTask extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         date.setText(sdf.format(myCalendar.getTime()));
     }
+
+
+//    private class TaskServer extends AsyncTask<String, String, String> {
+//        DataOutputStream dataOutputStrea(String... data) {
+//            try {
+//
+//            }
+//        }
+
 }
