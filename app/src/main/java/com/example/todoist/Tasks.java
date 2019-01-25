2d56bb090a292613ec39c4a6260e7889b2233eed
mahsafrhni <38652406+mahsafrhni@users.noreply.github.com>
2019-01-26 00:07:53 +0330

    Update Tasks.java

--- app/src/main/java/com/example/todoist/Tasks.java
+++ app/src/main/java/com/example/todoist/Tasks.java
@@ -1,14 +1,44 @@
 package com.example.todoist;
 
+import java.sql.Time;
+
 public class Tasks {
     String title;
-    String date_and_time;
+    String date;
+    Time time;
     String priority;
     String other;
+    int value; /* 0 : checkbox disable, 1 : checkbox enable */
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+//    public String toStringDate(Date d) {
+//       return d.toString();
+//
+//    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 
     public Tasks(String t, String d, String p, String o) {
+        //super();
         title = t;
-        date_and_time = d;
+        date = d;
         priority = p;
         other = o;
     }
@@ -25,7 +55,79 @@ public class Tasks {
         this.other = other;
     }
 
-    public void setDate_and_time(String date_and_time) {
-        this.date_and_time = date_and_time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+//    public static Comparator<Tasks> getTaskDateComperator() {
+//        return taskDateComperator;
+//    }
+
+    public String date() {
+        return date;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+
+    //    public int compareTo(Tasks compareTask) {
+//return getDate().compareTo(compareTask.getDate());
+////        EditText compareQuantity = ((Tasks) compareTask).date();
+//        //ascending order
+//      //  return this.Tasks - compareQuantity;
+//
+//        //descending order
+//        //return compareQuantity - this.quantity;
+//
+//    }
+//    Collections.sort(myList, new Comparator<MyObject>() {
+//        public int compare(MyObject o1, MyObject o2) {
+//            return o1.getDateTime().compareTo(o2.getDateTime());
+//        }
+//    });
+//
+//    public static Comparator<Tasks> taskDateComperator=new Comparator<Tasks>() {
+//        @Override
+//        public int compare(Tasks o1, Tasks o2) {
+//            String date1=o1.getDate().
+//
+//        }
+//    }
+//}
+//
+    public String compare(Tasks t1, Tasks t2) {
+        String sooner = null;
+//    String compareQuantity = ((Tasks) t1).getDate();
+        if (t1.getDate().compareTo(t2.getDate()) > 0) {
+//date2 is sooner
+            sooner = t2.getDate();
+        }
+        if (t1.getDate().compareTo(t2.getDate()) < 0) {
+//date1 is later
+            sooner = t1.getDate();
+        }
+        if (t1.getDate().compareTo(t2.getDate()) == 0) {
+//same time
+            sooner = t1.getDate();
+
+        }
+        //ascending order
+        //return this.date - compareQuantity;
+
+        //descending order
+        //return compareQuantity - this.quantity;
+        return sooner;
+
     }
 }
