0f383e2c72c50f544589c936338f25107e157629
dena99 <38652032+dena99@users.noreply.github.com>
2019-01-25 21:24:02 +0330

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,29 +6,28 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
-    static ArrayList<ClientHandler> clientHandlers;
-    static ArrayList<String> username;
-    static ArrayList<String> password;
-    static ArrayList<String> email;
+
+    //static ArrayList<ClientHandler> clientHandlers;
+    static ArrayList<User> users;
     static ArrayList<String> session;
     static int[] sn;
+    static int numOfClients;
 
     public static void main(String[] args) throws IOException {
-        clientHandlers = new ArrayList<>();
-        username = new ArrayList<>();
-        password = new ArrayList<>();
-        session = new ArrayList<>();
+        //clientHandlers = new ArrayList<>();
+        users = new ArrayList<>();
         sn = new int[30];
-        ServerSocket serverSocket = new ServerSocket(1234);
+        ServerSocket serverSocket = new ServerSocket(5000);
         Socket socket;
         try {
             while (true) {
                 socket = serverSocket.accept();
-                //System.out.println("Connection established!");
+                System.out.println("Connection established!");
                 ClientHandler clientHandler = new ClientHandler(socket);
                 Thread thread = new Thread(clientHandler);
-                clientHandlers.add(clientHandler);
+                //clientHandlers.add(clientHandler);
                 thread.start();
+               // numOfClients++;
             }
         } catch (IOException e) {
             e.printStackTrace();
